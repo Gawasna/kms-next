dd1d630c47c47f5616ea798e071443d09cadb8b2
Gawasna <hunglepy05@gmail.com>
2025-09-08 20:28:31 +0700

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,6 @@
+// lib/db.ts
 import { PrismaClient } from '@prisma/client';
 
-// Đảm bảo rằng biến toàn cục 'prisma' chỉ được định nghĩa một lần
-// Đây là một mẫu thiết kế cho hot-reloading trong phát triển Next.js
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
+const prisma = new PrismaClient();
 
 export default prisma;
